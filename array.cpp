a0714dd34ad707eea78ed85b733f703df4fcffbd
Tom <tom@contextfree.info>
2020-12-30 07:04:01 -0700

    Write vectors

--- array.cpp
+++ array.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <array>
 #include <cstdlib>
+#include <iostream>
 #include <numeric>
+#include <ranges>
 
 using Scalar = double;
 
@@ -24,6 +26,17 @@ auto operator+(const Vector<Size>& a, const Vector<Size>& b) -> Vector<Size> {
   return sum;
 }
 
+template<size_t Size>
+auto operator<<(std::ostream& out, const Vector<Size>& a) -> std::ostream& {
+  for (const auto& x: a | std::ranges::views::take(1)) {
+    out << x;
+  }
+  for (const auto& x: a | std::ranges::views::drop(1)) {
+    out << " " << x;
+  }
+  return out;
+}
+
 template<size_t NRow, size_t NCol>
 // using Matrix = Scalar[NRow][NCol];
 using Matrix = std::array<Vector<NCol>, NRow>;
@@ -41,4 +54,5 @@ auto main() -> int {
   // Scalar a[2][3] = {{1, 2, 3}, {4, 5, 6}};
   auto a = Matrix<2, 3>{{{1, 2, 3}, {4, 5, 6}}};
   auto b = sum_across_rows(a);
+  std::cout << b << std::endl;
 }

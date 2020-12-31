0bec61ecea9dcb618d41d77ba2275972bd9e5edf
Tom <tom@contextfree.info>
2020-12-31 09:25:41 -0700

    Start tensor

--- tensor.cpp
+++ tensor.cpp
@@ -2,11 +2,28 @@
 #include <memory>
 #include <vector>
 
+// Dense only, not sparse.
 template<typename Val>
 struct Tensor {
-  std::vector<size_t> shape;
-  // Ownership?!?!? Views?!?!?
+  Tensor(const std::vector<size_t>& shape): sizes{shape} {
+    // TODO Allocate vals.
+  }
+
+  auto rank() {return sizes.size();};
+
+  auto operator[](std::initializer_list<size_t> coord) {
+    return vals[index(coord)];
+  }
+
+private:
+  size_t offset;
+  std::vector<size_t> sizes;
+  std::vector<size_t> strides;
   std::shared_ptr<std::vector<Val>> vals;
+
+  auto index(std::initializer_list<size_t> coord) -> size_t {
+    return 0;
+  }
 };
 
 auto main() -> int {

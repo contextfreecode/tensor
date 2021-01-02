d60588e47b8af84d3793115534fcca0f3673e890
Tom <tom@contextfree.info>
2021-01-02 11:26:57 -0700

    Punt vals to vector

--- tensor.cpp
+++ tensor.cpp
@@ -11,30 +11,33 @@ struct Tensor {
     sizes{shape},
     strides(sizes.size())
   {
+    // Calculate strides in row majorish order back to front.
+    // Imperative seems easier here at the moment and good enough.
     size_t stride = 1;
     for (size_t i = sizes.size(); i; i -= 1) {
       strides[i - 1] = stride;
       stride *= sizes[i - 1];
     }
-    vals = std::make_shared<Val[]>(stride);
+    // Final "stride" is for the whole array size.
+    vals = std::make_shared<std::vector<Val>>(stride);
   }
 
   auto rank() {return sizes.size();};
 
   auto operator[](std::initializer_list<size_t> coord) {
     std::cout << "index: " << index(coord) << std::endl;
-    return vals[index(coord)];
+    return vals->at(index(coord));
   }
 
 private:
   size_t offset = 0;
   std::vector<size_t> sizes;
   std::vector<size_t> strides;
-  std::shared_ptr<Val[]> vals;
+  std::shared_ptr<std::vector<Val>> vals;
 
   auto index(std::initializer_list<size_t> coord) -> size_t {
     return std::transform_reduce(
-      coord.begin(), coord.end(), strides.begin(), offset
+      coord.begin(), coord.end(), strides.cbegin(), offset
     );
   }
 };

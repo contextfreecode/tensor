e4e3e116aec1ccf9079cb804835e6d19fbc71895
Tom <tom@contextfree.info>
2021-01-02 12:00:10 -0700

    Work toward init list

--- tensor.cpp
+++ tensor.cpp
@@ -7,19 +7,28 @@
 // Dense only, not sparse.
 template<typename Val>
 struct Tensor {
-  Tensor(const std::vector<size_t>& shape):
-    sizes{shape},
-    strides(sizes.size())
-  {
+  Tensor() {
+    //
+  }
+
+  Tensor(std::initializer_list<Val> v) {
+    //
+  }
+
+  static auto zeros(const std::vector<size_t>& shape) -> Tensor<Val> {
+    auto result = Tensor<Val>();
+    result.sizes = shape;
+    result.strides.resize(shape.size());
     // Calculate strides in row majorish order back to front.
     // Imperative seems easier here at the moment and good enough.
     size_t stride = 1;
-    for (size_t i = sizes.size(); i; i -= 1) {
-      strides[i - 1] = stride;
-      stride *= sizes[i - 1];
+    for (size_t i = result.sizes.size(); i; i -= 1) {
+      result.strides[i - 1] = stride;
+      stride *= result.sizes[i - 1];
     }
     // Final "stride" is for the whole array size.
-    vals = std::make_shared<std::vector<Val>>(stride);
+    result.vals = std::make_shared<std::vector<Val>>(stride);
+    return result;
   }
 
   auto rank() {return sizes.size();};
@@ -43,7 +52,7 @@ private:
 };
 
 auto main() -> int {
-  auto a = Tensor<double>({2, 3});
+  auto a = Tensor<double>::zeros({2, 3});
   std::cout << a[{0, 0}] << std::endl;
   std::cout << a[{1, 2}] << std::endl;
 }

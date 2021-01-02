9ad311fb3629f75760729d9396ef4e57a68d1d14
Tom <tom@contextfree.info>
2021-01-02 07:18:58 -0700

    Tweak c

--- array.c
+++ array.c
@@ -3,19 +3,23 @@
 int main(void) {
   double a[][3] = {{1, 2, 3}, {4, 5, 6}};
   double* b[] = {(double[]){1, 2, 3}, (double[]){4, 5, 6}};
-  printf("sizeof a / sizeof(double)\n");
+  printf("sizeof _ / sizeof(double)\n");
   printf("%zu\n", sizeof a / sizeof(double));
   printf("%zu\n", sizeof b / sizeof(double));
-  printf("(char*)a[0] - (char*)a\n");
+  printf("(double*)_[0] - (double*)_\n");
   printf("%zu\n", (double*)a[0] - (double*)a);
   printf("%zu\n", (double*)b[0] - (double*)b);
-  printf("(char*)a[1] - (char*)a\n");
+  printf("(double*)_[1] - (double*)_\n");
   printf("%zu\n", (double*)a[1] - (double*)a);
   printf("%zu\n", (double*)b[1] - (double*)b);
-  printf("a[1] - a[0]\n");
+  printf("_[1] - _[0]\n");
   printf("%zu\n", a[1] - a[0]);
   printf("%zu\n", b[1] - b[0]);
-  printf("&a[0][1] - &a[0][0]\n");
+  printf("&_[0][1] - &_[0][0]\n");
   printf("%zu\n", &a[0][1] - &a[0][0]);
   printf("%zu\n", &b[0][1] - &b[0][0]);
+  printf("Contents\n");
+  for (size_t i = 0; i < sizeof a / sizeof(double); i += 1) {
+    printf("%lf %lf\n", ((double*)a)[i], ((double*)b)[i]);
+  }
 }

57c96be1cebc33be78e5bf5bff3dd8f9b360128c
Tom <tom@contextfree.info>
2021-01-01 21:06:43 -0700

    Measure things

--- array.c
+++ array.c
@@ -3,6 +3,19 @@
 int main(void) {
   double a[][3] = {{1, 2, 3}, {4, 5, 6}};
   double* b[] = {(double[]){1, 2, 3}, (double[]){4, 5, 6}};
-  printf("%zu\n", sizeof(a));
-  printf("%zu\n", sizeof(b));
+  printf("sizeof a / sizeof(double)\n");
+  printf("%zu\n", sizeof a / sizeof(double));
+  printf("%zu\n", sizeof b / sizeof(double));
+  printf("(char*)a[0] - (char*)a\n");
+  printf("%zu\n", (double*)a[0] - (double*)a);
+  printf("%zu\n", (double*)b[0] - (double*)b);
+  printf("(char*)a[1] - (char*)a\n");
+  printf("%zu\n", (double*)a[1] - (double*)a);
+  printf("%zu\n", (double*)b[1] - (double*)b);
+  printf("a[1] - a[0]\n");
+  printf("%zu\n", a[1] - a[0]);
+  printf("%zu\n", b[1] - b[0]);
+  printf("&a[0][1] - &a[0][0]\n");
+  printf("%zu\n", &a[0][1] - &a[0][0]);
+  printf("%zu\n", &b[0][1] - &b[0][0]);
 }

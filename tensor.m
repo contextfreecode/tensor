8eed0b8166b9850d6bc164607c6d2994b7b6f0ab
Tom <tom@contextfree.info>
2021-01-27 22:06:29 -0700

    Explore more octave

--- tensor.m
+++ tensor.m
@@ -4,6 +4,8 @@ function tensor
         99 89 82
     ];
     scores(2, 3, 1, 1, 1, 1)
+    reshape(scores, [1 fliplr(size(scores))])
+    reshape_ndims = ndims(reshape(scores, [1 size(scores, 2) size(scores, 1)]))
     mean_by_student = mean(scores, 1)
     ndims(mean_by_student)
     mean_by_test = mean(scores, 2)
